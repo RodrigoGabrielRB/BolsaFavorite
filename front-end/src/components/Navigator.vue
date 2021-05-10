8cb437429f13cfb78aea77dee80eb3ae173e1517
RodrigoGabrielRB <rodrigorgrb@gmail.com>
2021-05-09 21:06:26 -0300

    Feat: navigator bar in mobile

---
+++ front-end/src/components/Navigator.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="navigator">
+    <!-- Criar componente inteligente para filtrar -->
+    <i class="fas fa-chevron-left"></i>Minha conta
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Navigator"
+}
+</script>
+
+<style lang="scss" scoped>
+.navigator{
+    background: white;
+    color: #18acc4;
+    font-weight: bold;
+    padding: 20px 15px;
+    i{
+        padding-right: 10px;
+    }
+}
+</style>
